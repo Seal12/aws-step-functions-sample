97748a37a7efcb80ad1e909850baa8f21eed3395
Seale Rapolai <seale.rapolai@vcontractor.co.za>
2023-08-08 12:46:45 +0200

    Update workOnCaseFunction code

--- src/workOnCaseFunction.js
+++ src/workOnCaseFunction.js
@@ -1,10 +1,11 @@
 exports.handler = async function(event) {
   // Generate a random number to determine whether the support case has been resolved, then return that value along with the updated message.
-  var min = 0;
-  var max = 1;    
-  var myCaseStatus = Math.floor(Math.random() * (max - min + 1)) + min;
-  var myCaseID = event.Case;
-  var myMessage = event.Message;
+  const min = 0;
+  const max = 1;    
+  const myCaseStatus = Math.floor(Math.random() * (max - min + 1)) + min;
+  const myCaseID = event.Case;
+  let myMessage = event.Message;
+
   if (myCaseStatus == 1) {
       // Support case has been resolved    
       myMessage = myMessage + "resolved... ";
@@ -12,7 +13,8 @@ exports.handler = async function(event) {
       // Support case is still open
       myMessage = myMessage + "unresolved... ";
   } 
-  var result = {Case: myCaseID, Status : myCaseStatus, Message: myMessage};
+
+  const result = {Case: myCaseID, Status : myCaseStatus, Message: myMessage};
  
   return result;
 };
